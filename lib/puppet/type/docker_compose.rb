51b415a9fabd54ec624186881e27da1580d090c9
Rahul Hada <rahul.hada@criterionnetworks.com>
2020-04-11 12:02:44 +0530

    added support for the service name in docker compose

--- lib/puppet/type/docker_compose.rb
+++ lib/puppet/type/docker_compose.rb
@@ -2,11 +2,10 @@ Puppet::Type.newtype(:docker_compose) do
   @doc = 'A type representing a Docker Compose file'
 
   ensurable
-
-  def refresh
-      provider.restart
+  # Added parameter to support service_name
+  newparam(:service_name) do
+    desc 'This specify the name of the service, the values are fatched from the underlay'
   end
-
   newparam(:name) do
     desc 'Docker compose file path.'
   end
@@ -28,13 +27,6 @@ Puppet::Type.newtype(:docker_compose) do
     desc 'Additional options to be passed directly to docker-compose.'
 		validate do |value|
       fail 'options should be a String' unless value.is_a? String
-		end
-	end
-
-	newparam(:up_args) do
-    desc 'Arguments to be passed directly to docker-compose up.'
-		validate do |value|
-      fail 'up_args should be a String' unless value.is_a? String
 		end
 	end
 
